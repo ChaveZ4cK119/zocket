4c1d763a59b8455b6e983d745fdf6a8dbd5c474c
unknown <juanmanuelt307@gmail.com>
2024-02-26 21:38:21 -0500

    Se ha agregado las vistas

---
+++ view/register_product.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+
+<body>
+
+    <div class="register">
+        <form action="">
+            <label>Nombre del producto</label><br>
+            <input type="text" name="product"><br>
+            <label>Descripcion del prodicto</label><br>
+            <input type="text" name="description"><br>
+            <label>Codigo de producto</label><br>
+            <input type="number"><br>
+            <label>Precio del producto</label><br>
+            <input type="number"><br>
+            <input class="button" type="submit" value=guardar><br>
+
+        </form>
+    </div>
+
+</body>
+
+</html>
---
+++ view/register_user.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+
+<body>
+
+    <div class="register">
+        <h1>REGISTRO DE USUARIO</h1>
+        <form action="" method="POST">
+            <Label>Nombre</Label><br>
+            <input name="name" type="text"><br>
+            <label>Apellido</label><br>
+            <input name="surname" type="text"><br>
+            <label>Numero de documento</label><br>
+            <input name="identification" type="number"><br>
+            <input class="button" type="submit" value=guardar><br><br>
+        </form>
+        <a href="register_product.php"></a>
+    </div>
+
+</body>
+
+</html>
